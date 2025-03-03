3deafa77749498afb52cf6aa7b1b4839599a5dab
Clang Robot <robot@example.com>
2025-03-03 15:20:38 +0000

    :art: Applied clang-format to source code (auto-formatting) - Updated .h, .cpp, and .hpp files to conform with the style guide. - Changes include:   - Indentation adjustments   - Spacing improvements   - Alignment enhancements   - Consistency in formatting across files   - Removal of trailing whitespace

--- src/ogl/App.hpp
+++ src/ogl/App.hpp
@@ -1,7 +1,7 @@
 /*
-* Created by gbian on 03/03/2025.
-* Copyright (c) 2025 All rights reserved.
-*/
+ * Created by gbian on 03/03/2025.
+ * Copyright (c) 2025 All rights reserved.
+ */
 
 #pragma once
 #include "Window.hpp"
